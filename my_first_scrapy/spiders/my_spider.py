9571318b30af9028c949246440dd96be0916a9ba
nightFuryman <michaeljohnshane@Gmail.com>
2019-09-09 18:12:18 +0500

    my first spider

---
+++ my_first_scrapy/spiders/my_spider.py
@@ -0,0 +1,14 @@
+import scrapy
+
+class MySpider(scrapy.Spider):
+    name = "my_spider"
+    start_urls = [
+        'http://quotes.toscrape.com/page/1/',
+    ]
+
+    def parse(self, response):
+        for quote in response.css('div.quote'):
+            yield {
+                'text': quote.css('span.text::text').get(),
+                'author': quote.css('small.author::text').get(),
+            }
